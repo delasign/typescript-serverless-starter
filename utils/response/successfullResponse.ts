1834daef8f56630b842ab8d34a4d676625bae7c2
Oscar de la Hera Gomez <oscar@delasign.com>
2022-12-26 12:07:52 +0100

    Update Succesful response

--- utils/response/successfullResponse.ts
+++ utils/response/successfullResponse.ts
@@ -2,9 +2,13 @@
 import response from "utils/response/response";
 import LogComplete from "utils/logs/complete";
 // MARK: Function
-const SuccessfulResponse = (callback: any, process: string) => {
-  LogComplete(process);
-  return response(callback, 200, null, process + " Complete");
+const SuccessfulResponse = (callback: any, process: string, object?: any) => {
+  LogComplete(process, object);
+  if (object) {
+    return response(callback, 200, null, object);
+  } else {
+    return response(callback, 200, null, process + " Complete");
+  }
 };
 
 export default SuccessfulResponse;
